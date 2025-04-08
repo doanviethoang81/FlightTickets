611b23e1178ace1ee6597bf23f7bfb05cf1480b2
Doan Viet Hoang <hoangdoanviet81@gmail.com>
2025-04-08 23:15:58 +0700

    update 10

--- src/main/java/com/example/banvemaybay/configs/SecurityConfigs.java
+++ src/main/java/com/example/banvemaybay/configs/SecurityConfigs.java
@@ -18,6 +18,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,32 +39,54 @@ import java.util.Set;
 @EnableWebSecurity
 public class SecurityConfigs {
 
-    private final String client_id  = System.getProperty("GOOGLE_CLIENT_ID");
+    private final String client_id = System.getProperty("GOOGLE_CLIENT_ID");
     private final String client_secret = System.getProperty("GOOGLE_CLIENT_SECRET");
 
     private final CustomUserDetailService customUserDetailService;
-
-
     private final JWTUtil jwtUtil;
+
     @Autowired
     private NguoiDatRepository nguoiDatRepository;
 
     @Autowired
-    private RoleRepostiory roleRepostiory;
+    private RoleRepostiory roleRepository; // Sửa typo từ RoleRepostiory
 
     public SecurityConfigs(CustomUserDetailService customUserDetailService, JWTUtil jwtUtil) {
         this.customUserDetailService = customUserDetailService;
         this.jwtUtil = jwtUtil;
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder()
-    {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // Thêm bean ClientRegistrationRepository
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(googleClientRegistration());
+    }
+
+    // Cấu hình thông tin Google OAuth2
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId(client_id) // Lấy từ System.getProperty
+                .clientSecret(client_secret) // Lấy từ System.getProperty
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://localhost:8080/login/oauth2/code/google") // Local URL, sửa khi deploy lên Render
+                .scope(OidcScopes.OPENID, "email", "profile")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName("sub")
+                .clientName("Google")
+                .build();
     }
 
     @Bean
@@ -70,15 +98,15 @@ public class SecurityConfigs {
                         .requestMatchers("/api/v1/admin/**").permitAll()
                         .requestMatchers("/api/v1/posts/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
-//                        .requestMatchers("/api/v1/adminn/**").hasAuthority("ROLE_ADMIN") // Giới hạn quyền admin
                         .anyRequest().authenticated()
                 )
                 .cors(Customizer.withDefaults())
-                .addFilterBefore(new JWTAuthenticationFilter(jwtUtil,customUserDetailService), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(jwtUtil, customUserDetailService), UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("http://localhost:5173", true) // Điều hướng đến trang chủ sau khi đăng nhập thành công
-                        .successHandler(authenticationSuccessHandler()) // Xử lý gửi thông tin người dùng
+                        .clientRegistrationRepository(clientRegistrationRepository()) // Sử dụng bean vừa định nghĩa
+                        .defaultSuccessUrl("http://localhost:5173", true) // Local URL, sửa khi deploy
+                        .successHandler(authenticationSuccessHandler())
                 );
         return http.build();
     }
@@ -87,26 +115,21 @@ public class SecurityConfigs {
         return (request, response, authentication) -> {
             OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
             OAuth2User user = token.getPrincipal();
-            // Lấy thông tin người dùng
-            String email = user.getAttribute("email");  // Lấy email
-            String firstName = user.getAttribute("name");  // Lấy tên (first name)
-            String avatar = user.getAttribute("picture");  // Lấy hình ảnh (avatar)
+            String email = user.getAttribute("email");
+            String firstName = user.getAttribute("name");
+            String avatar = user.getAttribute("picture");
 
             Optional<NguoiDat> existingUser = nguoiDatRepository.findByEmail(email);
 
             if (existingUser.isPresent()) {
                 NguoiDat nguoiDat = existingUser.get();
-
-                // Nếu tài khoản này đã đăng ký bằng LOCAL, không thay đổi provider
                 if ("LOCAL".equals(nguoiDat.getProvider())) {
-                    String redirectUrlerror = "http://localhost:5173?error=" +
+                    String redirectUrlError = "http://localhost:5173?error=" +
                             URLEncoder.encode("Tài khoản này đã đăng ký bằng email vui lòng đăng nhập bằng mật khẩu!", "UTF-8");
                     response.setHeader("Content-Type", "text/html; charset=UTF-8");
-                    response.sendRedirect(redirectUrlerror);
+                    response.sendRedirect(redirectUrlError);
                     return;
                 }
-
-                // Nếu tài khoản đã đăng ký bằng GOOGLE, cập nhật thông tin mới nhất
                 nguoiDat.setTen(firstName);
                 nguoiDatRepository.save(nguoiDat);
             } else {
@@ -116,24 +139,21 @@ public class SecurityConfigs {
                 nguoiDat.setTen(firstName);
                 nguoiDat.setEnable(true);
                 nguoiDat.setProvider("GOOGLE");
-                Role userRole = roleRepostiory.findByName("ROLE_USER")
+                Role userRole = roleRepository.findByName("ROLE_USER")
                         .orElseGet(() -> {
                             Role newRole = new Role();
                             newRole.setName("ROLE_USER");
-                            return roleRepostiory.save(newRole);
+                            return roleRepository.save(newRole);
                         });
 
                 nguoiDat.setRoles(Set.of(userRole));
                 nguoiDatRepository.save(nguoiDat);
             }
-            String tokenJWT = jwtUtil.generateToken(email, Set.of("ROLE_USER")); // Tạo token JWT với email và role của người dùng
-
-            // Chuyển thông tin người dùng vào URL (ví dụ: email, first name, avatar)
-            String redirectUrl = "http://localhost:5173?name=" + URLEncoder.encode(
-                    firstName, "UTF-8")
+            String tokenJWT = jwtUtil.generateToken(email, Set.of("ROLE_USER"));
+            String redirectUrl = "http://localhost:5173?name=" + URLEncoder.encode(firstName, "UTF-8")
                     + "&avatar=" + avatar
                     + "&email=" + email
-                    + "&token=" + tokenJWT; // Thêm token vào URL
+                    + "&token=" + tokenJWT;
             response.setHeader("Content-Type", "text/html; charset=UTF-8");
             response.sendRedirect(redirectUrl);
         };
